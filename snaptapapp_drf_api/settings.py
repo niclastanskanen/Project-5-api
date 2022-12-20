fefe5f088429559ae82dc26bceefeca282394c47
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-20 21:27:21 +0000

    add new keys in env.py

--- snaptapapp_drf_api/settings.py
+++ snaptapapp_drf_api/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_8#-gx83xyvy^b=-3x=@9=lyd=ks=cea91_n-fw_jo1#%e&d6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
