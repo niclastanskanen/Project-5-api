228375a9d40b4c61a1af393eda2f6e07c67aeef8
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-25 09:09:14 +0000

    add comments urls path

--- snaptapapp_drf_api/settings.py
+++ snaptapapp_drf_api/settings.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = [
 
     'profiles',
     'posts',
+    'comments',
 
 ]
 
--- snaptapapp_drf_api/urls.py
+++ snaptapapp_drf_api/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     ),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
+    path('', include('comments.urls')),
 ]
