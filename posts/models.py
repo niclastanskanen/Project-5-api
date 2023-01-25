2de81ddd5ee65233ae0c4c4215f0361cbf219b05
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-25 09:02:40 +0000

    add posts models

---
+++ posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    image_filter_choices = [
+        ('art', 'Art'), ('cats', 'Cats'),
+        ('dogs', 'Dogs'), ('food', 'Food'),
+        ('nature', 'Nature'), ('photo', 'Photo'),
+        ('travel', 'Travel'), ('wallpaper', 'Wallpaper')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_h8c5hq', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
